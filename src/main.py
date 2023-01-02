72f2e0f41145f7be9b80c0262113cd70a9be8890
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-01-02 21:02:35 +0100

    Symulacja napisana przez Bartka

--- src/main.py
+++ src/main.py
@@ -1,1 +1,113 @@
-print('Hello World')
+import sys
+import time
+
+from PySide6.QtCore import Qt, QObject, Signal, Slot, QThread
+from PySide6.QtWidgets import QApplication, QMainWindow, QStyleOption, QStyle
+from PySide6.QtGui import QPaintEvent, QPainter, QPen, QBrush
+
+from kolejki import BestFirst, Fifo, PlaceFirst, WindowFirst, RowFirst  # type: ignore
+from pasazer import Pasazer
+
+
+class Worker(QObject):
+    data = Signal(list)
+
+    def __init__(self, dt: float = 0.5, interval: float = 0.005) -> None:
+        super(Worker, self).__init__()
+        self.dt = dt
+        self.interval = interval
+
+    def simulate(self) -> None:
+        time_elapsed = 0
+        passengers: list[Pasazer] = RowFirst(150, 1, 6)
+        # np.random.shuffle(Pasazer.passengers)
+        # print(passengers)
+        while passengers:
+            time_elapsed += self.dt  # jezeli tutaj to reakcja pasazerow jest instant
+            # jezli w for loop to pasazer ma swoj czas reakcji
+            time.sleep(self.interval)
+            x_positions = []
+            for passenger in passengers:
+                passenger.move(self.dt)
+                if passenger.stan == "siedzi":
+                    passengers.remove(passenger)
+                x_positions.append([passenger.x_pos, 0])
+            self.data.emit(passengers)
+        self.data.emit(passengers)
+        print(time_elapsed)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent=None):
+        super(MainWindow, self).__init__(parent)
+        # Create the circle widget and add it to the layout
+        global app
+        self.dt = 0.5
+        # size = app.primaryScreen().size() * 0.6
+        self.resize(600, 400)
+        self.setMinimumSize(600, 338)
+        self.x_positions = []
+        # symulacja
+        self.thread = QThread()
+        self.worker = Worker(0.5, 0.01)
+        self.worker.data.connect(self.update_positions)
+        self.worker.moveToThread(self.thread)
+        self.thread.started.connect(self.worker.simulate)  # type: ignore
+        self.thread.finished.connect(self.thread.deleteLater)  # type: ignore
+        self.thread.start()
+        # self.setCentralWidget(self.circle_widget)
+
+    @Slot(list)
+    def update_positions(self, data: list):
+        """Slot na odbieranie pozycji z wątku"""
+        # print('received')
+        self.x_positions = data
+        self.update()
+
+    def paintEvent(self, event: QPaintEvent) -> None:
+        """Re-implement paintEvent method"""
+        # print('u')
+        width, height = self.size().width(), self.size().height()
+        opt = QStyleOption()
+        opt.initFrom(self)
+        painter = QPainter(self)
+        self.style().drawPrimitive(QStyle.PrimitiveElement.PE_Widget, opt, painter, self)
+        multiplier = 20
+        shift = 5
+        for passenger in self.x_positions:
+            if passenger.stan == 'temp':
+                painter.setPen(QPen(Qt.green, 4, Qt.SolidLine))
+                painter.setBrush(QBrush(Qt.green, Qt.SolidPattern))
+            else:
+                painter.setPen(QPen(Qt.red, 4, Qt.SolidLine))
+                painter.setBrush(QBrush(Qt.red, Qt.SolidPattern))
+            x_pos = passenger.x_pos
+            # 30m = width
+            # # max x_pos = 30
+            #
+            # 0.1m = 1 px, 1m = 10px
+            x_pos = multiplier * x_pos
+            print(x_pos)
+            if x_pos < 0:
+                y_pos = height * 0.5 - abs(x_pos)
+                x_pos = shift * 0.5
+                print(y_pos)
+            else:
+                y_pos = height * 0.5
+
+            painter.drawEllipse(x_pos, y_pos - 2, 4, 4)
+        painter.setPen(QPen(Qt.black, 2, Qt.SolidLine))
+        painter.setBrush(QBrush(Qt.black, Qt.SolidPattern))
+        for i in range(25, 0, -1):
+            seat_pos = 0.3 + i * 0.8
+            position = int(seat_pos * multiplier + shift)
+            painter.drawRect(position, height * 0.5 - 18, 3, 10)
+            painter.drawRect(position, height * 0.5 + 10, 3, 10)
+        painter.end()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec())
--- src/pasazer.py
+++ src/pasazer.py
@@ -1,5 +1,13 @@
+from __future__ import annotations
 from numpy.random import normal
-class Pasazer:
+from miejsca import Miejsca
+
+
+class Samolot:
+    miejsca = Miejsca()
+
+
+class Pasazer(Samolot):
     """
     Klasa pasażera
     :param int chod_sr: Średnia prędkość chodu pasażera w metrach na sekundę
@@ -9,7 +17,8 @@ class Pasazer:
     :ivar chod_pr: Prędkość danego pasażera określona z rozkładu normalnego (chod_sr, chod_od) w metrach na sekundę
     :ivar wstw_cz: Czas wstawania danego pasażera określona z rozkładu normalnego (wstw_sr, wstw_od) w sekunadch
     :ivar siad_cz: Czas siadania danego pasażera określona z rozkładu normalnego (siad_sr, siad_od) w sekunadch
-    :ivar rozp_cz: Czas rozpakowywania się danego pasażera określona z rozkładu normalnego (rozp_sr, rozp_od) w sekunadch
+    :ivar rozp_cz: Czas rozpakowywania się danego pasażera \
+    określona z rozkładu normalnego (rozp_sr, rozp_od) w sekunadch
     :ivar stan: Określa aktualny stan danego pasażera
     :ivar czas_akcji: Parametr służący do okreslenia ile jeszcze czasu zajmie danemu pasażerowi określona w stanie akcja
     :type chod_pr: float
@@ -19,6 +28,7 @@ class Pasazer:
     :type stan: basestring
     :type czas_akcji: float
     """
+    passengers: list[Pasazer] = []
 
     def __init__(self, chod_sr, wstw_sr, pozycja, miejsce):
         """
@@ -45,9 +55,69 @@ class Pasazer:
 
         self.pozycja = pozycja
         self.miejsce = miejsce
+        self.stan = "temp"
         self.czas_akcji = 0
 
-    def __whileNormal__(self, srednia, odchylenie, zmienna=-1):
+        # zmiany
+        # x_pos - pozycja w korytarzu samolotu
+        self.x_pos = -self.pozycja
+        print(self.x_pos)
+        self.seat_at = self.seat_pos(self.miejsce)  # x_pos na którym znajduje się miejsce pasażera
+
+        self.passengers.append(self)
+        self.idx = self.passengers.index(self)
+
+    def move(self, dt):
+        """
+        Metoda odpowiedzialna za ruch pasażera
+        """
+        self.czas_akcji -= dt
+        self.czas_akcji = max(self.czas_akcji, 0)
+        if self.stan == 'siada':
+            if self.czas_akcji <= 0:
+                self.passengers.remove(self)
+                self.stan = 'siedzi'
+            return
+        if self.can_sit() and self.stan != 'siada':
+            self.sit()
+            return
+
+        if self.can_move(dt):
+            self.x_pos += dt * self.chod_pr
+
+    def can_sit(self) -> bool:
+        return self.x_pos > self.seat_at
+
+    def sit(self):
+        self.czas_akcji = max(self.czas_akcji, 0)
+        self.czas_akcji += self.miejsca.sit_at(self.miejsce, self)
+        # self.x_pos = self.seat_at
+        self.stan = 'siada'
+
+    def can_move(self, dt) -> bool:
+        # troche chiny, trzeba znalezc inny sposob
+        if self.czas_akcji > 0:
+            return False
+        try:
+            index = self.passengers.index(self)  # duzo czasu to zajmuje
+            if index - 1 < 0:
+                return self.czas_akcji <= 0 # True
+            return self.passengers[index - 1].x_pos - (self.x_pos + dt * self.chod_pr) > 0.3
+        except (IndexError, ValueError):
+            return self.czas_akcji <= 0 # True
+
+    def __str__(self):
+        return f'{self.idx}: {self.miejsce} {self.stan}, {self.x_pos}, {self.seat_at}, {self.czas_akcji}'
+
+    def __repr__(self):
+        return f'{self.idx}: {self.miejsce} {self.stan}, {self.x_pos}, {self.seat_at}, {self.czas_akcji}\n'
+
+    @staticmethod
+    def seat_pos(num) -> int:
+        return 0.3 + ((150 - num) // 6) * 0.8
+
+    @staticmethod
+    def __whileNormal__(srednia, odchylenie, zmienna=-1):
         while zmienna < 0:
             zmienna = normal(srednia, odchylenie)
         return zmienna
